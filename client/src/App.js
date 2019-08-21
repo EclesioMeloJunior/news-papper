75e88b65055af4845d09032e0f27fd86e7bc210b
eclesiomelojunior <eclesiomelo.1@gmail.com>
2019-08-20 22:10:41 -0400

    client side works

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import TopNews from "./components/TopNews";
 
 function App() {
-	return <div className="App" />;
+	return (
+		<React.Fragment>
+			<Container>
+				<Header />
+				<Categories />
+				<TopNews />
+			</Container>
+		</React.Fragment>
+	);
 }
 
 export default App;
